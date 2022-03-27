eadefe465944706198c652783ced1a5dd084cebc
root <root@ip-172-31-1-88.us-west-2.compute.internal>
2022-03-27 11:26:27 +0000

    my new project webapp

---
+++ .bash_history
@@ -0,0 +1,2 @@
+yum install -y java-1.8.0-openjdk-devel.x86_64
+sudo su
---
+++ royalenfield/src/main/java/www/re/com/App.java
@@ -0,0 +1,13 @@
+package www.re.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
